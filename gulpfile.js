417a91df3e5a670b881c61ef4a8897db39b1401d
Tristan Strathearn <r3oath@gmail.com>
2017-08-06 18:13:39 +1000

    Sets up repo with initial working commit.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var stripCssComments = require('gulp-strip-css-comments');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var processors = [autoprefixer({ browsers: ['last 2 versions'] })];
+
+gulp.task('build-dev', function() {
+  return gulp
+    .src('./src/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('buzuki.css'))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('build-prod', ['build-dev'], function() {
+  return gulp
+    .src('./bin/buzuki.css')
+    .pipe(stripCssComments({ preserve: false }))
+    .pipe(postcss(processors))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('build', ['build-dev', 'build-prod']);
+
+gulp.task('default', ['build']);
